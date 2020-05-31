ce81717bd3b925afbea456461709bba835556bcc
Aryansh Omray <aryansh.omray@outlook.com>
2020-06-01 04:33:35 +0530

    z

--- FSRCNN.py
+++ FSRCNN.py
@@ -50,16 +50,26 @@ class FSRCNN(nn.Module):
 
 class Image_Dataset(torch.utils.data.Dataset):
 
-    def __init__(self, scale_factor):
+    def __init__(self, scale_factor, train = True, filename = None):
+
+        self.train = train
         self.scale_factor = scale_factor
-        self.filenames = [os.path.join('Data', img_name) for img_name in os.listdir('Data')]
+
+        if self.train:
+            self.filenames = [os.path.join('Data', img_name) for img_name in os.listdir('Data')]
+        else:
+            self.filenames = [os.path.join('testdata',filename, img_name) for img_name in os.listdir(os.path.join('testdata', filename))]
 
     def __len__(self):
         return len(self.filenames)
 
     def __getitem__(self, idx):
 
-        HR = joblib.load(self.filenames[idx])
+        if self.train:
+            HR = joblib.load(self.filenames[idx])
+        else:
+            HR = cv2.imread(self.filenames[idx])
+
         width, height, _ = HR.shape
         LR = cv2.resize(HR, (width//self.scale_factor, height//self.scale_factor))
         
@@ -68,15 +78,18 @@ class Image_Dataset(torch.utils.data.Dataset):
 
         return LR, HR
 
-def train(scale_factor, batch_size, num_workers, epochs, lr = 1e-3):
 
-    FILENAME = f'FSRCNN_{scale_factor}x_BS_{batch_size}_workers_{num_workers}_epochs_{epochs}_lr{lr}_{np.random.random()}'
-    os.mkdir(os.path.join('Logs', FILENAME))
 
+def train(scale_factor, batch_size, num_workers, epochs, lr = 1e-3, device = 'cpu'):
+    #Tensorboard Summary Writer Initialization
     writer = SummaryWriter(log_dir=os.path.join('Logs',FILENAME))
+
     Dataset = Image_Dataset(scale_factor = scale_factor)
     Dataloader = torch.utils.data.DataLoader(Dataset, batch_size, False, num_workers=num_workers)
-    model = FSRCNN(scale_factor=scale_factor)
+
+    model = FSRCNN(scale_factor=scale_factor).to(device)
+    model.train()
+
     optimizer = torch.optim.Adam(model.parameters(), lr)
 
     writer.add_scalar('Scale_Factor', scale_factor)
@@ -86,6 +99,9 @@ def train(scale_factor, batch_size, num_workers, epochs, lr = 1e-3):
 
     for epoch in range(epochs):
         for idx, (input, target) in enumerate(Dataloader):
+
+            input = input.to(device)
+            target = target.to(device)
 
             if not epoch and not idx:
                 writer.add_graph(model, input)
@@ -109,5 +125,72 @@ def train(scale_factor, batch_size, num_workers, epochs, lr = 1e-3):
 
     writer.close()
 
+def test(scale_factor, num_workers, path):
+
+    testfiles = os.listdir('testdata')
+    
+    for testfile in testfiles:
+        
+        writer = SummaryWriter(log_dir=os.path.join('Logs',FILENAME,'Test',testfile))
+        Dataset = Image_Dataset(scale_factor = scale_factor, False, testfile)
+        Dataloader = torch.utils.data.DataLoader(Dataset, 1, False, num_workers=num_workers)
+
+        model = FSRCNN(scale_factor=scale_factor)
+        model.load_state_dict(torch.load(path))
+        mode.eval()
+
+        writer.add_scalar('Scale_Factor', scale_factor)
+
+        with torch.no_grad():
+            for idx, (input, target) in enumerate(Dataloader):
+
+                input = input.to(device)
+                target = target.to(device)
+
+
+                output = model(input)
+                loss = F.mse_loss(output, target)
+
+                psnr = PSNR(target.detach(), output.detach())
+                ssim = SSIM(target.detach(), output.detach())
+
+                writer.add_scalar('Loss', loss.item())
+                writer.add_scalar('Metric/PSNR', psnr)
+                writer.add_scalar('METRIC/SSIM', ssim)
+
+        writer.close()
+
+
 if __name__ == "__main__":
-    train(2,2,8,20)
+
+    p = argparse.ArgumentParser()
+
+    p.add_argument('scale_factor',default=2,type=int)
+    p.add_argument('epochs',default=100,type=int)
+    p.add_argument('workers',default=80,type=int)
+    p.add_argument('batch_size',default=256,type=int)
+    p.add_argument('lr',default=1e-3,type=float)
+    p.add_argument('device',default='cpu',type=str)
+    p.add_argument('checkpoint',default=None,type=str)
+    
+    args = p.parse_args()
+
+    FILENAME = f'FSRCNN_{scale_factor}x_BS_{batch_size}_workers_{num_workers}_epochs_{epochs}_lr{lr}_{np.random.random()}'
+    os.mkdir(os.path.join('Logs', FILENAME))
+
+    if args['checkpoint'] is None:
+        train(
+            args['scale_factor'],
+            args['batch_size'],
+            args['workers'],
+            args['epochs'],
+            args['lr'],
+            args['device']
+        )
+    else:
+        test(
+            args['scale_factor'],
+            args['workers'],
+            args['epochs'],
+            args['checkpoint']
+        )
